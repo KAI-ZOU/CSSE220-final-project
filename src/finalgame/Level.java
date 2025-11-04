ce52af6b1ce7c84957221a196f737850266f5cc1
zouks <zouks@137.112.206.118>
2025-11-04 14:10:30 -0500

    Merge branch 'main' of https://github.com/KAI-ZOU/CSSE220.git into main

--- src/finalgame/Level.java
+++ src/finalgame/Level.java
@@ -90,10 +90,13 @@ public class Level extends JPanel{
             objects.add(new Enemy(0, 450, 120, 100, 100, 3, 3, new String[]{""}, new Sprite[] {new Sprite(55, 55, "BlackBatYellow.png")}));
             objects.add(new Enemy(0, 250, 690, 100, 100, 5, 5, new String[]{""}, new Sprite[] {new Sprite(55, 55, "BlackBatYellow.png")}));
             objects.add(new Enemy(0, 650, 400, 200, 100, 7, 7, new String[]{""}, new Sprite[] {new Sprite(55, 55, "BlackBatYellow.png")}));
-objects.add(new Enemy(0, 950, 320, 100, 100, 3, 3, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
+            objects.add(new Enemy(0, 950, 320, 100, 100, 3, 3, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
             objects.add(new Enemy(0, 250, 990, 100, 100, 5, 5, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
             objects.add(new Enemy(0, 650, 400, 200, 100, 7, 7, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
 
+            objects.add(new Enemy(0, 550, 800, 100, 100, 0, 0, new String[]{""}, new Sprite[] {new Sprite(55, 55, "Spike.png")}));
+            objects.add(new Enemy(0, 950, 500, 100, 100, 0, 0, new String[]{""}, new Sprite[] {new Sprite(55, 55, "Spike.png")}));
+            platforms.add(new Platform(0, 550, 950, 0, 0, 0, 0, new String[]{""}, new Sprite[] {new Sprite(150, 15, "Blank-PNG-Pic.png")}));
             
         } else if (level == 2) {
             requiredscore = 100; 
@@ -113,12 +116,17 @@ objects.add(new Enemy(0, 950, 320, 100, 100, 3, 3, new String[]{""}, new Sprite[
             objects.add(new Enemy(0, 100, 545, 100, 100, 5, 5, new String[]{""}, new Sprite[] {new Sprite(55, 55, "BlackBatYellow.png")}));
             objects.add(new Enemy(0, 700, 545, 100, 100, 5, 5, new String[]{""}, new Sprite[] {new Sprite(55, 55, "BlackBatYellow.png")}));
             objects.add(new Enemy(0, 450, 200, 100, 100, 8, 8, new String[]{""}, new Sprite[] {new Sprite(55, 55, "BlackBatYellow.png")}));
-
             objects.add(new Enemy(0, 100, 545, 100, 100, 5, 5, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
             objects.add(new Enemy(0, 700, 545, 100, 100, 5, 5, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
             objects.add(new Enemy(0, 650, 300, 100, 100, 8, 8, new String[]{""}, new Sprite[] {new Sprite(55, 55, "enemyTest.png")}));
-
             
+        
+//            public Enemy(int id, int xPosition, int yPosition, int movementRangeX, int movementRangeY, int velocityX, int velocityY, String[] spriteNames, Sprite[] sprites) {
+//        		super(id, xPosition, yPosition, movementRangeX, movementRangeY, velocityX, velocityY, spriteNames, sprites);
+//        	}
+//            public Platform(int id, int xPosition, int yPosition, int movementRangeX, int movementRangeY, int velocityX, int velocityY, String[] spriteNames, Sprite[] sprites) {
+//        		super(id, xPosition, yPosition, movementRangeX, movementRangeY, velocityX, velocityY, spriteNames, sprites);
+//        	}
         } else {
             
             return; 
